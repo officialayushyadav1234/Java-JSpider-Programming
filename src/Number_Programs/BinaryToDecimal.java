0e5766d079536c1d747d3d01794ae228f51619d9
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-05-18 16:52:39 +0530

    Number Programs

---
+++ src/Number_Programs/BinaryToDecimal.java
@@ -0,0 +1,31 @@
+package Number_Programs;
+
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Binary Number: ");
+		int n = sc.nextInt();
+		getDecimal(n);
+	}
+
+	public static void getDecimal(int n) {
+		int sum = 0;
+		int twomul = 1;
+		while (n > 0) {
+			int rem = n % 10;
+			if (rem == 0 || rem == 1)
+				sum = sum + rem * twomul;
+			else {
+				System.out.println("This is Notb a valid Binary Number");
+				return;
+			}
+			twomul *= 2;
+			n = n / 10;
+		}
+		System.out.println("The decimal value is: " + sum);
+	}
+}
